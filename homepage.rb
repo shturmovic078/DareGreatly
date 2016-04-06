d3c2111fa8f1c909c0d3c282d9ae6313395a50d1
Earl <earl.willis@publicismodem.com>
2016-04-06 14:55:17 -0400

    homepage check links

---
+++ homepage.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'watir-webdriver'
+
+include Selenium
+
+caps = WebDriver::Remote::Capabilities.new
+caps[:os] = "Windows"
+caps[:name] = "Watir WebDriver"
+caps[:browser] = "firefox"
+caps[:browser_version] = "44"
+caps["browserstack.debug"] = "true"
+
+browser = Watir::Browser.new(:remote,
+  :url => "http://zaqwsx1:Fs54nwmULt7BaSTosZxi@hub.browserstack.com/wd/hub",
+  :desired_capabilities => caps)
+
+browser.goto "http://www.daregreatly.com"
+
+browser.button(:name => 'btnG').click
+
+puts browser.title
+browser.quit
