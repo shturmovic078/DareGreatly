629ebb92f4ee9db618c0b21c1e4f91a5ff9dde1b
Earl <earl.willis@publicismodem.com>
2016-04-11 13:35:13 -0400

    Axa
    Scripting

--- homepage.rb
+++ homepage.rb
@@ -1,5 +1,15 @@
-require 'rubygems'
 require 'watir-webdriver'
+require 'webdriver-user-agent'
+require 'csv'
+require 'watir-scroll'
+require 'time'
+
+
+
+
+ 
+
+def setupBrowserStack
 
 include Selenium
 
@@ -10,13 +20,59 @@ caps[:browser] = "firefox"
 caps[:browser_version] = "44"
 caps["browserstack.debug"] = "true"
 
-browser = Watir::Browser.new(:remote,
+$b= Watir::Browser.new(:remote,
   :url => "http://zaqwsx1:Fs54nwmULt7BaSTosZxi@hub.browserstack.com/wd/hub",
   :desired_capabilities => caps)
 
-browser.goto "http://www.daregreatly.com"
+
+$b.goto "http://yahoo.com"
+end 
 
 
 
-puts browser.title
-browser.quit
+puts setupBrowserStack
+
+i=0;
+csv_text = File.read('axa.csv',encoding: "iso-8859-1:UTF-8")
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|  
+sleep 4
+url= "#{row['url']}"
+$b.goto url
+
+csv_text = File.read('size.csv',encoding: "iso-8859-1:UTF-8")
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|  
+sleep 4
+loc1= "#{row['sizeA']}"
+loc2= "#{row['sizeB']}"
+loc3= "#{row['device']}"
+
+sleep 5
+puts loc1
+
+$b.window.resize_to(loc1, loc2)
+i+=1
+sleep 2
+
+$b.screenshot.save i.to_s<<"_"<<loc1<<"x"<<loc2<<device<<".png"
+
+
+sleep 2
+
+
+
+
+end
+
+end
+
+
+
+
+
+
+ 
+
+
+$b.quit
